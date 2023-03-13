d6f1a625839eb403d2cca10477abaf448b367414
Jack Herrington <jherr@pobox.com>
2023-03-13 11:18:58 -0700

    First commit

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -5,7 +5,7 @@ import {Title} from './Title';
 
 export const MyComposition = () => {
 	return (
-		<AbsoluteFill className="bg-gray-100 items-center justify-center">
+		<AbsoluteFill className=" items-center justify-center">
 			<div className="m-10" />
 			<Logo />
 			<div className="m-3" />
